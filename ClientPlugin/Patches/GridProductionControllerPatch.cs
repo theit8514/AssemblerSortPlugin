c2a2b88cca755aa1eea41f2f39c06292f200e2fe
theit8514 <theit8514@users.noreply.github.com>
2024-10-25 00:25:33 -0400

    Add project files and create assembler sort patch

---
+++ ClientPlugin/Patches/GridProductionControllerPatch.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.Localization;
+using Sandbox.Graphics.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using VRage;
+
+#pragma warning disable IDE0060
+// ReSharper disable UnusedParameter.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+namespace ClientPlugin.Patches
+{
+    [HarmonyPatch(ClassType, "Init")]
+    public class GridProductionControllerPatch
+    {
+        private const string ClassType = "Sandbox.Game.Gui.MyTerminalProductionController";
+        private static readonly FieldInfo ComboBoxFieldInfo;
+        private static readonly FieldInfo AssemblersByKeyFieldInfo;
+
+        static GridProductionControllerPatch()
+        {
+            ComboBoxFieldInfo = AccessTools.Field(AccessTools.TypeByName(ClassType), "m_comboboxAssemblers");
+            AssemblersByKeyFieldInfo = AccessTools.Field(AccessTools.TypeByName(ClassType), "m_assemblersByKey");
+        }
+
+        public static void Postfix(object __instance,
+            IMyGuiControlsParent controlsParent,
+            MyCubeGrid grid,
+            MyCubeBlock currentBlock)
+        {
+            if (grid == null)
+                return;
+
+            if (!(ComboBoxFieldInfo.GetValue(__instance) is MyGuiControlCombobox comboboxAssemblers))
+                return;
+
+            // Just sorts by name
+            //comboboxAssemblers.SortItemsByValueText();
+
+            if (!(AssemblersByKeyFieldInfo.GetValue(__instance) is Dictionary<int, MyAssembler> assemblersByKey))
+                return;
+
+            // For whatever reason this only works if performed twice.
+            //comboboxAssemblers.CustomSortItems((item1, item2) =>
+            //{
+            //    var assembler1 = assemblersByKey[(int) item1.Key];
+            //    var assembler2 = assemblersByKey[(int) item2.Key];
+            //    if (assembler1 == currentBlock)
+            //        return -1;
+            //    if (!assembler1.IsFunctional && assembler2.IsFunctional)
+            //        return 1;
+            //    return string.Compare(item1.Value.ToString(), item2.Value.ToString(), StringComparison.CurrentCulture);
+            //});
+
+            // This is the best method I've found so far:
+            // Rebuild the combobox by using m_assemblersByKey
+            var incompleteAssemblerName = new StringBuilder();
+            comboboxAssemblers.ClearItems();
+            foreach (var (key, myAssembler) in assemblersByKey.OrderBy(x =>
+                     {
+                         var myAssembler = x.Value;
+                         return myAssembler == currentBlock
+                             ? -1
+                             : myAssembler.IsFunctional ? 0 : 10000;
+                     }).ThenBy(x => x.Value.CustomName.ToString()))
+            {
+                if (!myAssembler.IsFunctional)
+                {
+                    incompleteAssemblerName.Clear();
+                    incompleteAssemblerName.AppendStringBuilder(myAssembler.CustomName);
+                    incompleteAssemblerName.AppendStringBuilder(
+                        MyTexts.Get(MySpaceTexts.Terminal_BlockIncomplete));
+                    comboboxAssemblers.AddItem(key, incompleteAssemblerName);
+                }
+                else
+                    comboboxAssemblers.AddItem(key, myAssembler.CustomName);
+            }
+
+            // Reset the selected index.
+            comboboxAssemblers.SelectItemByIndex(0);
+            Plugin.Instance.Log.Debug("Sorted assemblers!");
+        }
+    }
+}
--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -1,50 +1,43 @@
-﻿using System;
-using System.Reflection;
+﻿using ClientPlugin.Logging;
 using HarmonyLib;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using VRage.Plugins;
 
 namespace ClientPlugin
 {
     // ReSharper disable once UnusedType.Global
-    public class Plugin : IPlugin, IDisposable
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Plugin : IPlugin
     {
-        public const string Name = "PluginTemplate";
+        // ReSharper disable once MemberCanBePrivate.Global
+        public const string Name = "AssemblerSortPlugin";
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public static Plugin Instance { get; private set; }
 
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public IPluginLogger Log => Logger;
+        private static readonly IPluginLogger Logger = new PluginLogger(Name);
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public void Init(object gameInstance)
         {
             Instance = this;
 
-            // TODO: Put your one time initialization code here.
-            Harmony harmony = new Harmony(Name);
+            Log.Info("Starting AssemblerSortPlugin");
+
+            var harmony = new Harmony(Name);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
         public void Dispose()
         {
-            // TODO: Save state and close resources here, called when the game exits (not guaranteed!)
-            // IMPORTANT: Do NOT call harmony.UnpatchAll() here! It may break other plugins.
-
             Instance = null;
         }
 
         public void Update()
         {
-            // TODO: Put your update code here. It is called on every simulation frame!
         }
-
-        // TODO: Uncomment and use this method to create a plugin configuration dialog
-        // ReSharper disable once UnusedMember.Global
-        /*public void OpenConfigDialog()
-        {
-            MyGuiSandbox.AddScreen(new MyPluginConfigDialog());
-        }*/
-
-        //TODO: Uncomment and use this method to load asset files
-        /*public void LoadAssets(string folder)
-        {
-
-        }*/
     }
 }
